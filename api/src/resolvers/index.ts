13fcd7a124a092618f5186333af3ec5ad7559b22
Alexander Johansson <alexander@n1s.se>
2018-01-31 12:32:07 +0000

    🗺️ localize string clever util

--- api/src/resolvers/index.ts
+++ api/src/resolvers/index.ts
@@ -1,10 +1,49 @@
 import { extractFragmentReplacements } from 'prisma-binding';
-import { Exercise, Locale, LocalizedStringVariation } from '../generated/prisma';
+import { Exercise, Locale, LocalizedString, LocalizedStringVariation } from '../generated/prisma';
 import { Context } from '../utils';
 import { AuthPayload } from './AuthPayload';
 import { auth } from './Mutation/auth';
 import { post } from './Mutation/post';
 import { Query } from './Query';
+
+// TODO 🤔 prob not ideal to have hardcoded
+const fallbackLocale: Locale = 'En_GB';
+
+function localize(sourceName: string, fieldName: string) {
+  const fragment = `
+    fragment LocalizeFragment on ${sourceName} {
+      ${fieldName} {
+        variations {
+          id
+          string
+          locale
+        }
+      }
+    }
+  `;
+
+  return {
+    [`${fieldName}_t`] : {
+      fragment,
+      resolve: (source, args, context: Context, info) => {
+        const sourceString: LocalizedString = source[fieldName];
+        // FIXME CHANGEME
+        const userSelectedLocale: Locale = 'Sv_SE';
+
+        const primary = sourceString.variations.find(({ locale }) => locale === userSelectedLocale);
+        const fallback = primary ? null : sourceString.variations
+          .find(({ locale }) => locale === fallbackLocale) ||
+          sourceString.variations.find(({string: text}) => !!text);
+
+        return {
+          string: primary ? primary.string : fallback.string,
+          isFallback: !primary,
+          fallback,
+        };
+      },
+    },
+  };
+}
 
 export const resolvers = {
   Query,
@@ -25,26 +64,7 @@ export const resolvers = {
     },
   },
   Exercise: {
-    name_string: {
-      fragment: 'fragment ExerciseFragment on Exercise { name { variations { id, string, locale } } }',
-      resolve: (source: Exercise, args, context: Context, info) => {
-        console.log({source: JSON.stringify(source, null, 4)});
-        // FIXME CHANGEME
-        const userSelectedLocale: Locale = 'En_GB';
-        const fallbackLocale: Locale = 'En_GB';
-
-        const primary = source.name.variations
-          .find(({ locale }) => locale === userSelectedLocale);
-        const fallback = source.name.variations
-          .find(({ locale }) => locale === fallbackLocale);
-
-        return {
-          string: primary ? primary.string : fallback.string,
-          isFallback: !!primary,
-          fallback,
-        };
-      },
-    },
+    ...localize('Exercise', 'name'),
   },
 };
 export const fragmentReplacements = extractFragmentReplacements(resolvers);
